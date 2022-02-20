c00edaeccda6859a5343c0550028a98b253de182
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-02-20 18:05:06 +0100

    created sender and receiver

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 use std::thread;
 use std::fs;
+use std::sync::mpsc;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let (tx, rx): (mpsc::Sender<(String, i8)>, mpsc::Receiver<(String, i8)>) = mpsc::channel();
 
     let file1: String = args[1].to_string();
     let file2: String = args[2].to_string();
