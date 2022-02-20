90b13b33715732036f7c1c42abadcfe821a1b697
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-02-20 18:29:30 +0100

    fixed index out of range error

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,13 @@ fn main() {
     thread::spawn(move || {
         println!("Reading file 1");
         let content = fs::read_to_string(file1).expect("there was an error in file 1");
-        tx.send((content, 1)).unwrap();
+        tx.send((content, 0)).unwrap();
     });
     
     thread::spawn(move || {
         println!("Reading file 2");
         let content = fs::read_to_string(file2).expect("there was an error in file 2");
-        tx1.send((content, 2)).unwrap();
+        tx1.send((content, 1)).unwrap();
     });
 
     let mut contents: [String;2] = ["".to_string(), "".to_string()];
