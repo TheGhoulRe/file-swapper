b34e31fdc28c6e932ca11d4e75360765df698a87
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-02-20 17:56:34 +0100

    created first handle

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::env;
 use std::thread;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let file1 = &args[1];
-    let file2 = &args[2];
+    let file1: String = args[1].to_string();
+    let file2: String = args[2].to_string();
 
     let mut content1 = String::new();
     let mut content2 = String::new();
 
+    let handle1 = thread::spawn(move || {
+        println!("Reading file 1");
+        content1 = fs::read_to_string(file1).unwrap();
+        return content1;
+    });
 }
