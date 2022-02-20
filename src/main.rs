e218469aacef5ddc5d764f88bb7b14789b9a2ecb
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-02-20 17:50:01 +0100

    corrected args and packages

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use std::thread;
-use std::io::Read;
-use std::io::Write;
+use std::env;
 
 fn main() {
-    let mut cmd_line = std::env::args();
-    cmd_line.next();
+    let args: Vec<String> = env::args().collect();
 
-    let file1 = cmd_line.next();
-    let file2 = cmd_line.next();
+    let file1 = &args[1];
+    let file2 = &args[2];
 
     let mut content1 = String::new();
     let mut content2 = String::new();
