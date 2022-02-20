e889a32d695f520a6f939110c00ec104b0fa49ee
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-02-20 17:25:46 +0100

    created the command line args reading

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,10 @@ use std::io::Read;
 use std::io::Write;
 
 fn main() {
-    println!("Hello, world!");
+    let cmd_line = std::env::args();
+    let mut args: Vec<String> = vec![];
+
+    for arg in cmd_line {
+        args.push(arg.to_string());
+    }
 }
