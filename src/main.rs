04696a40d85430c600b9f2e19292aef333eab6a7
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-02-20 18:01:17 +0100

    fixed error in concurrency

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,14 @@ fn main() {
     let file1: String = args[1].to_string();
     let file2: String = args[2].to_string();
 
-    let mut content1 = String::new();
-    let mut content2 = String::new();
-
     let handle1 = thread::spawn(move || {
         println!("Reading file 1");
-        content1 = fs::read_to_string(file1).unwrap();
-        return content1;
+        let content = fs::read_to_string(file1).unwrap();
     });
 
     let handle2 = thread::spawn(move || {
         println!("Reading file 1");
-        content2 = fs::read_to_string(file2).unwrap();
-        return content2;
+        let content = fs::read_to_string(file2).unwrap();
     });
 
 }
